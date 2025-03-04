ab8500c72856b50c1e49261b3bb235fcadd2e0a7
luca0898 <lucaguiraldello@hotmail.com>
2025-03-04 11:55:25 -0300

    Ajusting CreateUserResult

--- src/Ambev.DeveloperEvaluation.Application/Users/CreateUser/CreateUserResult.cs
+++ src/Ambev.DeveloperEvaluation.Application/Users/CreateUser/CreateUserResult.cs
@@ -1,4 +1,7 @@
-﻿namespace Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+﻿using Ambev.DeveloperEvaluation.Domain.Models.UserAggregate.Common;
+using Ambev.DeveloperEvaluation.Domain.Models.UserAggregate.Enums;
+
+namespace Ambev.DeveloperEvaluation.Application.Users.CreateUser;
 
 public class CreateUserResult
 {
@@ -7,4 +10,44 @@ public class CreateUserResult
     /// </summary>
     /// <value>A GUID that uniquely identifies the created user in the system.</value>
     public Guid Id { get; set; }
+
+    /// <summary>
+    /// The user's email address
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The user's username
+    /// </summary>
+    public string Username { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The user's password
+    /// </summary>
+    public string Password { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The user's name details
+    /// </summary>
+    public NameDto Name { get; set; } = new NameDto();
+
+    /// <summary>
+    /// The user's address details
+    /// </summary>
+    public AddressDto Address { get; set; } = new AddressDto();
+
+    /// <summary>
+    /// The user's phone number
+    /// </summary>
+    public string Phone { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The current status of the user
+    /// </summary>
+    public UserStatus Status { get; set; }
+
+    /// <summary>
+    /// The user's role in the system
+    /// </summary>
+    public UserRole Role { get; set; }
 }
